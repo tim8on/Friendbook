3f24f9eb05587453d0d2ec216d9c936206674ebf
tim8on <tim8on@gmail.com>
2018-06-02 12:35:00 -0700

    yo rows

--- Friendbook/FriendListViewController.swift
+++ Friendbook/FriendListViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class FriendListViewController: UIViewController {
+//add "UITableViewDataSource" and "UITableViewDelegate"
+//need to answer how many rows and what's in each row
+class FriendListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    //control drag table view to connect it to the code
+    @IBOutlet weak var tableView: UITableView!
+    
     //Something that happens when the app first loads the first screen.
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //Do any additional setup after loading the view.
+        
+        //Saying that the data for the table is going to come from itself.
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+    
+    //how many rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    //what's in a row - aka a cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel!.text = "yo"
+        return cell
     }
     
 }
