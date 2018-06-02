69b10e61be9c21a917cca9739414c3311e1472f8
tim8on <tim8on@gmail.com>
2018-06-02 13:14:09 -0700

    built detail VC

---
+++ Friendbook/FriendDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FriendDetailViewController.swift
+//  Friendbook
+//
+//  Created by Tim Aton on 6/2/18.
+//  Copyright © 2018 Tim Aton. All rights reserved.
+//
+
+import UIKit
+
+class FriendDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
