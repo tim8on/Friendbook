1d3876734ede8a09aa16ed02009480ca537dee14
tim8on <tim8on@gmail.com>
2018-06-02 09:04:41 -0700

    Made my own view controller.

---
+++ Friendbook/FriendListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FriendListViewController.swift
+//  Friendbook
+//
+//  Created by Tim Aton on 6/2/18.
+//  Copyright © 2018 Tim Aton. All rights reserved.
+//
+
+import UIKit
+
+class FriendListViewController: UIViewController {
+
+    //Something that happens when the app first loads the first screen.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
